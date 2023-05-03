946b9e74b24142da336964775674820bd99fda06
Lily Zeiset <lilyzeiset@gmail.com>
2023-05-03 15:11:19 -0400

    CategoryService

---
+++ src/main/java/com/skillstorm/project/services/CategoryService.java
@@ -0,0 +1,60 @@
+package com.skillstorm.project.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.skillstorm.project.dtos.CategoryDto;
+import com.skillstorm.project.models.Category;
+import com.skillstorm.project.repositories.CategoryRepository;
+
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	/**
+	 * Finds a category by its id
+	 * @param id category id
+	 * @return The data of the found category
+	 */
+	public CategoryDto findCategoryById(long id) {
+		return categoryRepository.findById(id)
+				.orElseThrow()
+				.toDto();
+	}
+
+	/**
+	 * Finds all categories
+	 * @return List of categories
+	 */
+	public List<CategoryDto> findAllCategories() {
+		return categoryRepository.findAll()
+				.stream()
+				.map(w -> w.toDto())
+				.toList();
+	}
+
+	/**
+	 * Creates/updates a category
+	 * @param categoryData
+	 * @return the new category
+	 */
+	public CategoryDto createCategory(CategoryDto categoryData) {
+		Category category = new Category(
+				categoryData.getId(),
+				categoryData.getName(),
+				categoryData.getDesc());
+		
+		return categoryRepository.save(category).toDto();
+	}
+
+	/**
+	 * 
+	 * @param id
+	 */
+	public void deleteCategory(long id) {
+		categoryRepository.deleteById(id);
+	}
+
+}
